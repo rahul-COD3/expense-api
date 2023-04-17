dfa4c4362fd13d67f003950e2c12ace4c3a08b7a
Divu <d1403kr3@gmail.com>
2023-04-17 18:16:35 +0530

    Friends Model Added

---
+++ src/EMS.Domain/Friends/Friend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Users;
+
+namespace EMS.Friends
+{
+    public class Friend :AuditedAggregateRoot<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid FriendId { get; set; }
+        public bool IsDeleted { get; set; }
+        
+
+    }
+}
