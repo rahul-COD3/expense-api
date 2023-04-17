03a33266692db4895c6d60712090d7e4bb95882c
Divu <d1403kr3@gmail.com>
2023-04-17 12:29:08 +0530

    Allow users to invite friends via email for expense management

--- src/EMS.Application/Users/UserAppService.cs
+++ src/EMS.Application/Users/UserAppService.cs
@@ -7,26 +7,62 @@ using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 using Volo.Abp.Domain.Entities;
-
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Emailing;
+using Volo.Abp.Emailing.Templates;
+using Volo.Abp.TextTemplating;
+using System.Net.Mail;
 
 namespace EMS.Users
 {
-    public class UserAppService : ApplicationService, IUserAppService
+    public interface IUserAppService
+    {
+        Task<bool> CheckEmailExists(string email);
+    }
+
+    public class UserAppService : ApplicationService, IUserAppService, ITransientDependency
     {
         private readonly IIdentityUserRepository _userRepository;
+        private readonly IEmailSender _emailSender;
+        private readonly ITemplateRenderer _templateRenderer;
 
-        public UserAppService(IIdentityUserRepository userRepository)
+        public UserAppService(IIdentityUserRepository userRepository, IEmailSender emailSender, ITemplateRenderer templateRenderer)
         {
             _userRepository = userRepository;
+            _emailSender = emailSender;
+            _templateRenderer = templateRenderer;
         }
 
         public async Task<bool> CheckEmailExists(string email)
         {
             var users = await _userRepository.GetListAsync();
-            return users.Any(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+            bool emailExists = users.Any(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+
+            if (!emailExists)
+            {
+                await SendEmailAsync(email);
+            }
+
+            return emailExists;
         }
 
+        private async Task SendEmailAsync(string targetEmail)
+        {
+            var mailMessage = new MailMessage();
+            mailMessage.To.Add(targetEmail);
+            mailMessage.From = new MailAddress("test1403email@gmail.com");
+            mailMessage.Subject = "Join Splitwise";
+            mailMessage.Body = "Please join to Splitwise using this link https://secure.splitwise.com/login";
 
+            using (var smtpClient = new SmtpClient("smtp.gmail.com"))
+            {
+                smtpClient.Port = 587;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential("test1403email@gmail.com", "fohmxxrccvyyzcsl");
+                smtpClient.EnableSsl = true;
 
+                await smtpClient.SendMailAsync(mailMessage);
+            }
+        }
     }
 }
