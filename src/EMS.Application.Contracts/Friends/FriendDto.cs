e51a773b855b6948d46dcdc188eb0b8236eb44e1
Divu <d1403kr3@gmail.com>
2023-04-17 19:45:20 +0530

    Friend model and Controller is made

---
+++ src/EMS.Application.Contracts/Friends/FriendDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace EMS.Friends
+{
+    public class FriendDto : AuditedEntityDto<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid FriendId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
