077f44d86281f7fe92d94a32e49f438501965a2e
Krishn Mehta <krishnmehta11@gmail.com>
2023-04-20 18:00:57 +0530

    List of Payment for all the Users

--- src/EMS.Application/Payments/PaymentAppService.cs
+++ src/EMS.Application/Payments/PaymentAppService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Users;
 
 namespace EMS.Payments
 {
@@ -18,10 +23,11 @@ namespace EMS.Payments
         CreateUpdatePaymentDto>, 
     IPaymentAppService 
     {
+       
         public PaymentAppService(IRepository<Payment, Guid> repository)
             : base(repository)
         {
-
+            
         }
         //public override async Task<string> UpdateAsync(Guid id, CreateUpdatePaymentDto input)
         //{
@@ -42,6 +48,23 @@ namespace EMS.Payments
         //    // If the payment has not been settled, return an empty string in the API response
         //    return "";
         //}
+
+        [HttpGet]
+        [Route("api/payments/list")]
+        [Authorize]
+        public async Task<List<PaymentDto>> GetSettledPaymentsofCurrentUserAsync() // returns the list of payment for the current user
+        {
+            
+
+            var payments = await Repository.GetListAsync(p => p.IsSettled);
+            
+            if(payments.Count == 0)
+            {
+                throw new UserFriendlyException("No Transaction to Show");
+            }
+
+            return ObjectMapper.Map<List<Payment>, List<PaymentDto>>(payments);
+        }
 
     }
 }
