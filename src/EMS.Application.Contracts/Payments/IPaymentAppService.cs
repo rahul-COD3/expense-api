c1e4adc14fc4f64263f651d01fffb07687c03213
Krishn Mehta <krishnmehta11@gmail.com>
2023-04-22 16:51:47 +0530

    Auto Payment Feature Implemented

--- src/EMS.Application.Contracts/Payments/IPaymentAppService.cs
+++ src/EMS.Application.Contracts/Payments/IPaymentAppService.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace EMS.Payments
 {
-    public interface IPaymentAppService :
-    ICrudAppService< 
-        PaymentDto, 
-        Guid, 
-        PagedAndSortedResultRequestDto, 
-        CreateUpdatePaymentDto>
+    public interface IPaymentAppService : IApplicationService
     {
-
+        
     }
 }
