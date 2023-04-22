90ae4c374b11e115844f209a75d349e750e4d289
Divu <d1403kr3@gmail.com>
2023-04-22 16:12:44 +0530

    This commit will help user to get details of user who owes you, also tell you the name of the group

---
+++ src/EMS.Application.Contracts/Payments/PaymentYouGetDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMS.Payments
+{
+    public class PaymentYouGetDto
+    {
+        public Guid? OwesFromYou { get; set; }
+        public string? GroupName { get; set; }
+        public string? Message { get; set; }
+    }
+}
