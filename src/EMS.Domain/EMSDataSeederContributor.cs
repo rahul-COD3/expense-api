3908f0bf7a0eeb855ebab6b9a2260e36787bc1bf
rahulpromact <rahul@promactinfo.com>
2023-04-24 15:02:51 +0530

    seed data in group

--- src/EMS.Domain/EMSDataSeederContributor.cs
+++ src/EMS.Domain/EMSDataSeederContributor.cs
@@ -1,48 +1,73 @@
-﻿using EMS.Expenses;
+﻿using EMS.GroupMembers;
+using EMS.Groups;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
-using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
+using Volo.Abp.Identity;
+
 namespace EMS
 {
     public class EMSDataSeederContributor
     : IDataSeedContributor, ITransientDependency
     {
-        private readonly IRepository<Expense, Guid> _expenseRepository;
-        public EMSDataSeederContributor(IRepository<Expense, Guid> expenseRepository)
+        private readonly IGroupRepository _groupRepository;
+        private readonly GroupManager _groupManager;
+        private readonly IGroupMemberRepository _groupMemberRepository;
+        private readonly GroupMemberManager _groupMemberManager;
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly IdentityUserManager _userManager;
+        private readonly IIdentityUserRepository _userRepository;
+
+        public EMSDataSeederContributor(
+            IGroupRepository groupRepository,
+            GroupManager groupManager,
+            IGroupMemberRepository groupMemberRepository,
+            GroupMemberManager groupMemberManager,
+            IGuidGenerator guidGenerator,
+            IdentityUserManager userManager,
+            IIdentityUserRepository userRepository
+            )
         {
-            _expenseRepository = expenseRepository;
+            _groupRepository = groupRepository;
+            _groupManager = groupManager;
+            _groupMemberRepository = groupMemberRepository;
+            _groupMemberManager = groupMemberManager;
+            _guidGenerator = guidGenerator;
+            _userManager = userManager;
+            _userRepository = userRepository;
         }
         public async Task SeedAsync(DataSeedContext context)
         {
-            if (await _expenseRepository.GetCountAsync() <= 0)
+            if (await _groupRepository.GetCountAsync()<= 0)
             {
-                await _expenseRepository.InsertAsync(
-                    new Expense
-                    {
-                        expense_title = "ABC",
-                        expense_description = "XYZ",
-                        split_as = "PQR",
-                        currency = "Rupees",
-                        expense_amount = 2000,
-                    },
-                    autoSave: true
-                ); ;
-                await _expenseRepository.InsertAsync(
-                    new Expense
-                    {
-                        expense_title = "ABCQPR",
-                        expense_description = "XYZPQR",
-                        split_as = "PQRPQR",
-                        currency = "RupeesPQR",
-                        expense_amount = 2000,
-                    },
-                    autoSave: true
+                var rahul = new IdentityUser(_guidGenerator.Create(), "rahul", "rahul@gmail.com");
+                var adi = new IdentityUser(_guidGenerator.Create(), "adi", "adi@gmail.com");
+
+                await _userManager.CreateAsync(rahul , "1q2w3E*");
+                await _userManager.CreateAsync(adi , "1q2w3E*");
+
+                var group = await _groupManager.CreateAsync(
+                    "Dinner",
+                    "Dinner expense",
+                    rahul.Id,
+                    false
                 );
+                await _groupRepository.InsertAsync(group);
+
+                // Create a task for adding the current user as a member
+                var groupMembers = new List<Task<GroupMember>>();
+
+                var rahulM = _groupMemberManager.CreateAsync(rahul.Id, group.Id, false, DateTime.Now);
+                var adiM = _groupMemberManager.CreateAsync(adi.Id, group.Id, false, DateTime.Now);
+
+                groupMembers.Add(rahulM);
+                groupMembers.Add(adiM);
+
+                var newGroupMembers = await Task.WhenAll(groupMembers);
+                await _groupMemberRepository.InsertManyAsync(newGroupMembers);
             }
         }
     }
