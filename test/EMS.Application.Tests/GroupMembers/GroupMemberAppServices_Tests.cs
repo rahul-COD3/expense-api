3170d9566d6a7d4a401632f8482ae6a477b37e6e
rahulpromact <rahul@promactinfo.com>
2023-04-25 12:59:34 +0530

    add unit-testing

---
+++ test/EMS.Application.Tests/GroupMembers/GroupMemberAppServices_Tests.cs
@@ -0,0 +1,30 @@
+﻿using EMS.Groups;
+using Shouldly;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace EMS.GroupMembers
+{
+    public class GroupMemberAppServices_Tests : EMSApplicationTestBase
+    {
+        private readonly IGroupMemberAppService _groupMemberAppService;
+        private readonly IGroupAppService _groupAppService;
+
+        public GroupMemberAppServices_Tests()
+        {
+            _groupMemberAppService = GetRequiredService<IGroupMemberAppService>();
+            _groupAppService = GetRequiredService<IGroupAppService>();
+
+        }
+        [Fact]
+        public async Task Should_Get_List_Of_GroupMembers()
+        {
+            var result = await _groupMemberAppService.GetListAsync(
+                    new GetGroupMemberListDto()
+               );
+            result.TotalCount.ShouldBeGreaterThan(1);
+        }
+
+    }
+}
